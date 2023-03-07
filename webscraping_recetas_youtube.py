a62d413363ff38e4362caa89f9d421a5c8711632
Ignaciogg <nachogilgarzon@gmail.com>
2023-03-07 10:57:51 +0100

    conexion bbdd

--- webscraping_recetas_youtube.py
+++ webscraping_recetas_youtube.py
@@ -1,24 +1,71 @@
+# pip install google-api-python-client
+# pip install mysql.connector
+
 import requests
 import json
+import mysql.connector
+import datetime
+from pytube import YouTube
 
-# Introduce la url del canal de YouTube
-channel_url = input("Introduce la url del canal de YouTube: ")
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-# Extraer el ID del canal de la URL
+mydb = mysql.connector.connect(
+  host="",
+  user="",
+  passwd="",
+  database=""
+)
+
+mycursor = mydb.cursor()
+
+# 1. EXTRAER X URLs DE UN CANAL DE YOUTUBE
+channel_url = 'https://www.youtube.com/@SuperGustoso'
 channel_id = channel_url.split("/")[-1]
+yt_key = 'AIzaSyB0Z5YoVzZPgH1EOSVFJAL5X8EBb9qbSPU'
 
-# URL para obtener los últimos 5 videos de un canal
-# API Key: AIzaSyB0Z5YoVzZPgH1EOSVFJAL5X8EBb9qbSPU
-url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={channel_id}&maxResults=5&order=date&type=video&key=AIzaSyB0Z5YoVzZPgH1EOSVFJAL5X8EBb9qbSPU"
+url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={channel_id}&maxResults=1&order=date&type=video&key={yt_key}"
 
-# Realizar una solicitud GET a la URL
 response = requests.get(url)
-
-# Convertir la respuesta en un objeto JSON
 json_data = json.loads(response.text)
 
-# Recorrer los resultados y extraer la URL del video
 for item in json_data["items"]:
     video_id = item["id"]["videoId"]
     video_url = f"https://www.youtube.com/watch?v={video_id}"
     print(video_url)
+
+    # 2. COMPROBAR SI YA TENEMOS ESTA URL
+    mycursor.execute("SELECT * FROM receta WHERE url=?", (video_url,))
+    consultaURL = mycursor.fetchone()
+
+    if consultaURL is not None:
+        print("El URL ya existe en la tabla Recetas")
+    else:
+        print("El URL no existe en la tabla de recetas")
+
+        # 3. EXTRAER EL NUMERO DE COMENTARIOS DE ESTE VIDEO
+        youtubeCommentConnector = build('youtube', 'v3', developerKey=yt_key)
+        try:
+            response = youtubeCommentConnector.commentThreads().list(
+                part='snippet',
+                videoId=video_id
+            ).execute()
+
+            num_comentarios = response['pageInfo']['totalResults']
+
+        except HttpError as error:
+            print(f'Ha ocurrido un error: {error}')
+
+        # 4. AÑADIMOS MÁS ATRIBUTOS
+        yt = YouTube(video_url)
+        titulo = yt.title
+        autor = yt.author
+
+        fecha_actual = datetime.datetime.now().strftime('%Y-%m-%d')
+
+        # 5. INSERTAMOS EN LA BASE DE DATOS
+        sql = "INSERT INTO Receta (url, titulo, texto, categoria, comentarios, nutriscore, sentimiento, fecha_creacion) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+        val = (video_url, titulo, 'Aperitivo', num_comentarios, nutriscore, 1.5, fecha_actual)
+        mycursor.execute(sql, val)
+
+        mydb.commit()
